832e9748b596bb0dcea815ba5de62ffd7e8e2117
WeiChien Hsu <weichien711@gmail.com>
2019-11-24 01:48:41 -0800

    UI Enhancement
     ## App.js
    	- Add offlineSearch() function to search result in Cookie based on URL
    	- Update the Error Handler to setup server_error message
    	- Modify the position of Search, Refresh and Offline Search
    	- Add catch function to handle error message from a failed post request (400 Bad Request)
     ## Instruction.js
    	- Update the Instruction
     ## D3Tree.js
    	- And the handleOnHover() function and real-time URL info display column
    	- Add a new feature to transfer orientation
     ## Output.js
     	- Handle Error Message by reading the property "server_error"
    	sent from the App.js

--- webapp/react-front-end/src/components/Instructions.js
+++ webapp/react-front-end/src/components/Instructions.js
@@ -5,19 +5,27 @@ class Instructions extends Component {
     return (
       <>
         <h1 className="text-center">How it Works</h1>
-        <h4>BFS</h4>
+        <h4>BFS Search Method</h4>
         <p>
           The program will follow ALL links from the start page, and ALL links
           from each page it visits, until the crawler h- As reached the limit of
           pages deep, it should visit.
         </p>
-        <h4>DFS</h4>
+        <h4>DFS Search Method</h4>
         <p>
           The program will start at the start page, randomly choose one of the
           links on that page, then follow it to the next page. Then, on the next
           page, it randomly selects a link from the options available and
           follows it. This makes a chain from the starting page. This continues
           until the program hits the page limit indicated.
+        </p>
+        <h4>Offline Search</h4>
+        <p>
+        The program will not send the request directly to the backend; instead, 
+        it will search the cookies in the user's browser to explore whether there 
+        is a reaching result for the combination of the URL and Keyword users have 
+        been seeking before. The result will always be the latest one, no matter 
+        what depth the user has chosen, but the algorithm would matter.
         </p>
       </>
     );
